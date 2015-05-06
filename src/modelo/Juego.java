ca9f134d061fd173721365ae056cd906237e154a
Daniel E. Quijada Diaz <daniventf@gmail.com>
2015-05-06 13:32:38 +0100

    Ampliado esqueleto básico

---
+++ src/modelo/Juego.java
@@ -0,0 +1,29 @@
+/**
+ * sPAIce_invaders.modelo.Juego.java
+ * Proyecto Final: sPAIce invaders.
+ *
+ * Asignatura: Programación de Aplicaciones Interactivas.
+ * Universidad de La Laguna.
+ * Curso: 2014-2015
+ *
+ * Autores: 
+ *    Daniel E. Quijada Díaz
+ *    Francisco J. Palacios Rodríguez.
+ *    Héctor Rodríguez Pérez
+ */
+package modelo;
+
+
+/**
+ * TODO Descripción de la clase. Clase que contendrá todo el juego (lógico).
+ */
+public class Juego {
+
+   /**
+    * 
+    */
+   public Juego () {
+      // TODO Auto-generated constructor stub
+   }
+
+}
