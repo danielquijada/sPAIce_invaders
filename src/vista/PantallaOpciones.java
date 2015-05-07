9a064fbddbab8937ceba702e5cf1e340b335777b
Francisco Javier Palacios Rodriguez <alu0100503623@ull.edu.es>
2015-05-07 23:47:38 +0100

    Menu inicial

---
+++ src/vista/PantallaOpciones.java
@@ -0,0 +1,26 @@
+/**
+ * sPAIce_invaders.vista.PantallaOpciones.java
+ * Proyecto Final: sPAIce invaders.
+ *
+ * Asignatura: Programación de Aplicaciones Interactivas.
+ * Universidad de La Laguna.
+ * Curso: 2014-2015
+ *
+ * Autores: 
+ *    Daniel E. Quijada Díaz
+ *    Francisco J. Palacios Rodríguez.
+ *    Héctor Rodríguez Pérez
+ */
+package vista;
+
+/**
+ * TODO La pantalla de settings
+ */
+public class PantallaOpciones extends Pantalla {
+	   /**
+	    * 
+	    */
+	   public PantallaOpciones() {
+	      // TODO Auto-generated constructor stub
+	   }
+}
