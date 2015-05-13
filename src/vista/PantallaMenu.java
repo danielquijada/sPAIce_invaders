9f4d7f80f8c8a845898cb38cc5ab03b8a5ceae79
Francisco Javier Palacios Rodriguez <alu0100503623@ull.edu.es>
2015-05-13 13:06:33 +0100

    Menu principal + imagen de fondo
    Esta todo dibujado, le falta el listener

--- src/vista/PantallaMenu.java
+++ src/vista/PantallaMenu.java
@@ -16,10 +16,14 @@ package vista;
 import java.awt.Color;
 import java.awt.Font;
 import java.awt.Graphics;
+import java.awt.Image;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
 import java.util.Observable;
 import java.util.Observer;
 
-import modelo.Juego;
+import javax.imageio.ImageIO;
 
 
 /**
@@ -33,47 +37,71 @@ import modelo.Juego;
 @SuppressWarnings("serial")
 public class PantallaMenu extends Pantalla implements Observer {
 
-	public static final int OFFSETX = Juego.TOTAL_X / 2 - 150;  //Separacion desde el borde izquierdo
-	public static final int OFFSETY = Juego.TOTAL_Y / 2 - 250; //Separacion desde el borde superior
-	public static final int SEPARACION = 50; //Separacion vertical entre las opciones
+	private static final int OFFSETX = 100;
+	private static final int OFFSETY  = 30;
+	private static final int SEPARACION = 50; //Separacion vertical entre las opciones
 	
 	private static final int OFFSETX_SELECTOR = 20;
 	private static final int OFFSETY_SELECTOR = 17;
 	private static final int DIAMETRO = 15; //Tamaño del selector
 	
-	public static int selector = OFFSETY; //Posicion inicial del selector
+	private static final String RUTA = "./res/main_background.jpg";
+	
+	private BufferedImage image;
 	
    /**
     * 
     */
    public PantallaMenu () {
+	   setBackground(Color.BLACK);
 
+	   setImage(RUTA);
    }
 
 
 	protected void paintComponent(Graphics g) {
-		
-		   g.setColor(Color.BLACK);
-		   g.fillRect(0, 0, Juego.TOTAL_X, Juego.TOTAL_Y);
-		   g.setColor(Color.WHITE);
-		   g.setFont(new Font("Monospaced", Font.PLAIN, 30));
-		   g.drawString("Nuevo Juego", OFFSETX, OFFSETY);
-		   g.drawString("Cargar partida", OFFSETX, OFFSETY + SEPARACION);
-		   g.drawString("Opciones", OFFSETX, OFFSETY + (SEPARACION * 2));
-		   g.drawString("Salir", OFFSETX, OFFSETY + (SEPARACION * 3));
+	
+			super.paintComponent(g);
+			
+			int mitad_ancho = getWidth() / 2 - OFFSETX;
+			int mitad_alto = getHeight() / 2 - OFFSETY;
+			
+			g.drawImage(getImage().getScaledInstance(getWidth(), getHeight() / 2, Image.SCALE_SMOOTH), 0, 0, null);
+			g.setColor(Color.WHITE);
+			g.setFont(new Font("Monospaced", Font.PLAIN, 30));
+			g.drawString("Nuevo Juego", mitad_ancho, mitad_alto);
+			g.drawString("Cargar partida", mitad_ancho, mitad_alto + SEPARACION);
+			g.drawString("Opciones", mitad_ancho, mitad_alto + (SEPARACION * 2));
+			g.drawString("Salir", mitad_ancho, mitad_alto + (SEPARACION * 3));
 		   
-		   g.fillOval(OFFSETX - OFFSETX_SELECTOR, selector - OFFSETY_SELECTOR, DIAMETRO, DIAMETRO);
+			g.fillOval(mitad_ancho - OFFSETX_SELECTOR, mitad_alto - OFFSETY_SELECTOR, DIAMETRO, DIAMETRO);
 		   
 	}
 
 
-/* (non-Javadoc)
- * @see java.util.Observer#update(java.util.Observable, java.lang.Object)
- */
-@Override
-public void update(Observable o, Object arg) {
-	repaint();
+	/* (non-Javadoc)
+	 * @see java.util.Observer#update(java.util.Observable, java.lang.Object)
+	 */
+	@Override
+	public void update(Observable o, Object arg) {
+		repaint();
+		
+	}
 	
-}
+	
+	public BufferedImage getImage() {
+		return image;
+	}
+	
+	
+	public void setImage(String ruta) {
+		
+		try {
+			this.image = ImageIO.read(new File(ruta));
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	
+	}
 	
 }
