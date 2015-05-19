620fd3452af18216b2c8cb9aeef550697e580d8e
Francisco Javier Palacios Rodriguez <alu0100503623@ull.edu.es>
2015-05-19 17:07:53 +0100

    Tests

---
+++ src/tests/TestMovimientos.java
@@ -0,0 +1,38 @@
+/**
+ * sPAIce_invaders.tests.TestMovimientos.java
+ * Proyecto Final: sPAIce invaders.
+ *
+ * Asignatura: Programación de Aplicaciones Interactivas.
+ * Universidad de La Laguna.
+ * Curso: 2014-2015
+ *
+ * Autores: 
+ *    Daniel E. Quijada Díaz
+ *    Francisco J. Palacios Rodríguez.
+ *    Héctor Rodríguez Pérez
+ */
+package tests;
+
+import static org.junit.Assert.*;
+
+import org.junit.AfterClass;
+import org.junit.Test;
+
+/**
+ * TODO Descripción de la clase.
+ */
+public class TestMovimientos {
+
+	/**
+	 * @throws java.lang.Exception
+	 */
+	@AfterClass
+	public static void tearDownAfterClass() throws Exception {
+	}
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
